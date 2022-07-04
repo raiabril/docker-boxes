76710a6183045d720c5f9db7b9d8ee18e32618cf
Rai <rai@ingmechs.com>
2022-07-04 17:24:52 +0200

    Basic auth folder structure

--- docker/flask-basic-auth/src/app.py
+++ docker/flask-basic-auth/src/app.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+from flask import Flask
+app = Flask(__name__)
 
-import requests
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
-print(requests.get("https://www.google.com").status_code)
+
+if __name__ == "__main__":
+    app.run(debug=True)
