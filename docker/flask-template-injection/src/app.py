8f857475b4ebdad6f9e77edab3eca3ba2d693738
Rai <rai@ingmechs.com>
2022-07-04 17:35:29 +0200

    Working

--- docker/flask-template-injection/src/app.py
+++ docker/flask-template-injection/src/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, render_template_string
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,8 +8,13 @@ def hello_geek():
     
     user = request.args.get('user')
 
-    return render_template("index.html", user=user)
+    if user:
+        template = "Hello %s" % user
 
+    else:
+        template = f"Say the magic word!"
+
+    return render_template_string(template, user=user)
 
 if __name__ == "__main__":
     app.run(debug=True)
