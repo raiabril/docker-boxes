36555365161aab3764a080861ad6f6f4795f9274
Rai <rai@ingmechs.com>
2022-07-04 17:30:27 +0200

    Finished Basic auth

---
+++ docker/flask-basic-auth/README.md
@@ -0,0 +1,3 @@
+# Flask Basic Auth application
+
+This is a sample application using basic auth to learn brute forcing or whatever you need.
--- docker/flask-basic-auth/src/app.py
+++ docker/flask-basic-auth/src/app.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_basicauth import BasicAuth
+
 app = Flask(__name__)
+
+# Default values for the username and password
+app.config['BASIC_AUTH_USERNAME'] = 'john'
+app.config['BASIC_AUTH_PASSWORD'] = 'matrix'
+
+# Force basic auth in all the website
+app.config['BASIC_AUTH_FORCE'] = True
+
+# Initialize the basic auth
+basic_auth = BasicAuth(app)
+
 
 @app.route('/')
 def hello_geek():
